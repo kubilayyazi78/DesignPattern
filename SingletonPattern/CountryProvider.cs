665a46694d57bd5e602e2d627710e01a96df27bb
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-12-15 15:36:59 +0300

    SingletonPattern

---
+++ SingletonPattern/CountryProvider.cs
@@ -0,0 +1,22 @@
+﻿using SingletonPattern.Models;
+
+namespace SingletonPattern
+{
+    public class CountryProvider
+    {
+        private static CountryProvider instance = null;
+        public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+        private new List<Country> Countries { get; set; }
+        private CountryProvider()
+        {
+            Task.Delay(1000).GetAwaiter().GetResult();
+            Countries = new List<Country>()
+            {
+                new Country(){ Name="Türkiye"},
+                new Country(){ Name="ABD"}
+            };
+        }
+        public int CountryCount() => Countries.Count;
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonPattern;
+
+Console.WriteLine("Hello, World!");
+
+//var countryProvider = new CountryProvider();
+
+var countries = await CountryProvider.Instance.GetCountries();
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country.Name);
+}
+
+await CountryProvider.Instance.GetCountries();
+
+var x =  CountryProvider.Instance.CountryCount();
