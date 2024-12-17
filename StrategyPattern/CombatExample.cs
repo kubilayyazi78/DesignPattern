8371b6e76b68ba34155c9a09a56f25f4ccb2e791
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-12-17 21:31:27 +0300

    Strategy Pattern

---
+++ StrategyPattern/CombatExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    internal class CombatExample
+    {
+
+
+        class Character
+        {
+            private ICombatStrategy strategy;
+            public Character(ICombatStrategy combatStrategy)
+            {
+                strategy = combatStrategy;
+            }
+            public Character()
+            {
+
+            }
+            public void SetCombatStrategy(ICombatStrategy combatStrategy)
+            {
+                strategy = combatStrategy;
+            }
+            public void ApplyAttackStrategy()
+            {
+                strategy.Attack();
+            }
+        }
+        interface ICombatStrategy
+        {
+            void Attack();
+        }
+        class AggressiveCombatStrategy : ICombatStrategy
+        {
+            public void Attack()
+            {
+                Console.WriteLine("Agressive Attack");
+            }
+        }
+
+        class DefensiveCombatStrategy : ICombatStrategy
+        {
+            public void Attack()
+            {
+                Console.WriteLine("Defensive Attack");
+            }
+        }
+    }
+}
+//var ch = new Character(new AggressiveCombatStrategy());
+//ch.ApplyAttackStrategy();
+
+//ch.SetCombatStrategy(new DefensiveCombatStrategy());
+//ch.ApplyAttackStrategy();
+
+//Console.ReadLine();
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,99 @@
+﻿var paymentOptions = new PaymentOptions()
+{
+    CardNumber = "111",
+    CardHolderName = "ky",
+    ExpirationDate = "11.11",
+    Cvv = "123",
+    Amount = 1000
+};
+
+var paymentService = new PaymentService();
+do
+{
+    Console.WriteLine("ödeme yapılacak banka secin");
+    var bank = Console.ReadLine();
+
+    IPaymentService bankPaymentService = null;
+
+    switch (bank)
+    {
+        case "1":
+            bankPaymentService = new AkbankBankPaymentService();
+            break;
+        case "2":
+            bankPaymentService = new DenizBankPaymentService();
+            break;
+        case "3":
+            bankPaymentService = new GarantiBankPaymentService();
+            break;
+        default:
+            Console.WriteLine("geçersiz");
+            break;
+    }
+
+    paymentService.SetPaymentService(bankPaymentService);
+    paymentService.PayViaStrategy(paymentOptions);
+}
+while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+class PaymentService
+{
+    private IPaymentService _paymentService;
+
+    public PaymentService(IPaymentService paymentService)
+    {
+        _paymentService = paymentService;
+    }
+    public PaymentService()
+    {
+
+    }
+    public void SetPaymentService(IPaymentService paymentService)
+    {
+        _paymentService = paymentService;
+    }
+    public bool PayViaStrategy(PaymentOptions options)
+    {
+        return _paymentService.Pay(options);
+
+    }
+}
+public class GarantiBankPaymentService : IPaymentService
+{
+    public bool Pay(PaymentOptions options)
+    {
+        Console.WriteLine("Garanti ile ödeme");
+
+        return true;
+    }
+}
+public class AkbankBankPaymentService : IPaymentService
+{
+    public bool Pay(PaymentOptions options)
+    {
+        Console.WriteLine("Akbank ile ödeme");
+
+        return true;
+    }
+}
+public class DenizBankPaymentService : IPaymentService
+{
+    public bool Pay(PaymentOptions options)
+    {
+        Console.WriteLine("Deniz ile ödeme");
+
+        return true;
+    }
+}
+interface IPaymentService
+{
+    bool Pay(PaymentOptions options);
+}
+public class PaymentOptions
+{
+    public string CardNumber { get; set; }
+    public string CardHolderName { get; set; }
+    public string ExpirationDate { get; set; }
+    public string Cvv { get; set; }
+    public decimal Amount { get; set; }
+}
