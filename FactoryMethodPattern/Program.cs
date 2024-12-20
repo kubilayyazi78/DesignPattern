0e01a8825c8116ca2e332b0c2b126feca833b841
kubilayyazi78 <kubilayyazi@hotmail.com>
2024-12-20 17:40:30 +0300

    Factory Method Pattern

---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,88 @@
+﻿PizzaStore karabuk = new KarabukPizzaStore();
+PizzaStore istanbul = new IstanbulPizzaStore();
+
+IPizza cheesePizza = karabuk.OrderPizza("cheese");
+
+interface IPizza
+{
+    void Prepare();
+    void Bake();
+    void Cut();
+}
+
+class CheesePizza : IPizza
+{
+    public void Bake()
+    {
+        Console.WriteLine("CheesePizza Bake");
+    }
+
+    public void Cut()
+    {
+        Console.WriteLine("CheesePizza Cut");
+    }
+
+    public void Prepare()
+    {
+        Console.WriteLine("CheesePizza Prepare");
+    }
+}
+
+class VeggiPizza : IPizza
+{
+    public void Bake()
+    {
+        Console.WriteLine("VeggiPizza Bake");
+    }
+
+    public void Cut()
+    {
+        Console.WriteLine("VeggiPizza Cut");
+    }
+
+    public void Prepare()
+    {
+        Console.WriteLine("VeggiPizza Prepare");
+    }
+}
+
+abstract class PizzaStore
+{
+    protected abstract IPizza CreatePizza(string type);
+    public IPizza OrderPizza(string type)
+    {
+        IPizza pizza = CreatePizza(type);
+        pizza.Prepare();
+        pizza.Bake();
+        pizza.Cut();
+
+        return pizza;
+    }
+
+}
+
+class KarabukPizzaStore : PizzaStore
+{
+    protected override IPizza CreatePizza(string type)
+    {
+        return type switch
+        {
+            "cheese" => new CheesePizza(),
+            "veggi" => new VeggiPizza(),
+            _ => throw new ArgumentException("Invalid", nameof(type))
+        };
+    }
+}
+
+class IstanbulPizzaStore : PizzaStore
+{
+    protected override IPizza CreatePizza(string type)
+    {
+        return type switch
+        {
+            "cheese" => new CheesePizza(),
+            "veggi" => new VeggiPizza(),
+            _ => throw new ArgumentException("Invalid", nameof(type))
+        };
+    }
+}
